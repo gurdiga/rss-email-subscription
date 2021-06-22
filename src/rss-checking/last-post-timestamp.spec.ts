51037773aa654494178486a7c70753fd46985cb5
Vlad GURDIGA <gurdiga@gmail.com>
2021-06-22 10:33:08 +0300

    Flesh out some of getLastPostTimestamp

--- src/rss-checking/last-post-timestamp.spec.ts
+++ src/rss-checking/last-post-timestamp.spec.ts
@@ -1,11 +1,54 @@
 import { expect } from 'chai';
+import { makeDataDir, ValidDataDir } from './data-dir';
 import { getLastPostTimestamp } from './last-post-timestamp';
 
-// TODO: Introduce a Path type for dataDir, so that I can guarantee that
-// I can’t pass an invalid string into `getLastPostTimestamp`.
+describe(getLastPostTimestamp.name, () => {
+  const aTimestamp = new Date();
+  const dataDirPathString = '/some/path';
+  const dataDir = makeDataDir(dataDirPathString) as ValidDataDir;
 
-describe('getLastPostTimestamp', () => {
-  it('works', () => {
-    expect(getLastPostTimestamp).to.exist;
+  it('returns a ValidTimestamp value with the date recorded in lastPostTimestamp.json in dataDir', () => {
+    let actualPathArg = '';
+
+    const mockFileReaderFn = (filePath: string): string => {
+      actualPathArg = filePath;
+
+      const mockFileContent = JSON.stringify({ lastPostTimestamp: aTimestamp });
+      return mockFileContent;
+    };
+    const result = getLastPostTimestamp(dataDir, mockFileReaderFn);
+
+    expect(result).to.deep.equal({
+      kind: 'ValidTimestamp',
+      value: aTimestamp,
+    });
+
+    expect(actualPathArg).to.equal(`${dataDirPathString}/lastPostTimestamp.json`);
   });
+
+  it('returns an InvalidTimestamp value when can’t read lastPostTimestamp.json', () => {
+    const mockFileReaderFn = (_filePath: string) => {
+      throw new Error('Some IO error?!');
+    };
+    const result = getLastPostTimestamp(dataDir, mockFileReaderFn);
+
+    expect(result).to.deep.equal({
+      kind: 'InvalidTimestamp',
+      reason: `Can’t read ${dataDirPathString}/lastPostTimestamp.json: Some IO error?!`,
+    });
+  });
+
+  it('returns an InvalidTimestamp value when lastPostTimestamp.json does not contain valid JSON', () => {
+    const mockFileReaderFn = (_filePath: string) => {
+      return 'not a valid JSON string';
+    };
+    const result = getLastPostTimestamp(dataDir, mockFileReaderFn);
+
+    expect(result).to.deep.equal({
+      kind: 'InvalidTimestamp',
+      reason: `Invalid JSON in ${dataDirPathString}/lastPostTimestamp.json`,
+    });
+  });
+
+  // TODO: test for missing timestamp in JSON
 });
--- src/rss-checking/last-post-timestamp.ts
+++ src/rss-checking/last-post-timestamp.ts
@@ -1,5 +1,49 @@
+import path from 'path';
+import { readFileSync } from 'fs';
 import { ValidDataDir } from './data-dir';
 
-export function getLastPostTimestamp(dataDir: ValidDataDir): Date {
-  return new Date();
+interface ValidTimestamp {
+  kind: 'ValidTimestamp';
+  value: Date;
+}
+
+interface InvalidTimestamp {
+  kind: 'InvalidTimestamp';
+  reason: string;
+}
+
+type DataReaderFn = (filePath: string) => string;
+
+export function getLastPostTimestamp(
+  dataDir: ValidDataDir,
+  dataReaderFn: DataReaderFn = dataReader
+): ValidTimestamp | InvalidTimestamp {
+  const filePath = path.resolve(dataDir.value, 'lastPostTimestamp.json');
+
+  try {
+    const jsonString = dataReaderFn(filePath);
+
+    try {
+      const data = JSON.parse(jsonString);
+
+      return {
+        kind: 'ValidTimestamp',
+        value: new Date(data.lastPostTimestamp),
+      };
+    } catch (jsonParsingError) {
+      return {
+        kind: 'InvalidTimestamp',
+        reason: `Invalid JSON in ${filePath}`,
+      };
+    }
+  } catch (ioError) {
+    return {
+      kind: 'InvalidTimestamp',
+      reason: `Can’t read ${filePath}: ${ioError.message}`,
+    };
+  }
+}
+
+function dataReader(path: string) {
+  return readFileSync(path, 'utf-8');
 }
