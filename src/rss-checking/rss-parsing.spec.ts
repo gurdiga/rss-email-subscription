e8aba68bd72968e5dae12a3c2b263c34dc2e877d
Vlad GURDIGA <gurdiga@gmail.com>
2021-06-28 19:42:42 +0300

    buildRssItem: InvalidRssItem for invalid input

--- src/rss-checking/rss-parsing.spec.ts
+++ src/rss-checking/rss-parsing.spec.ts
@@ -11,19 +11,20 @@ describe(parseRssItems.name, () => {
     const expectedItems: RssItem[] = [
       {
         title: 'Serial post Sat Jun 12 19:04:59 EEST 2021',
-        content: '',
+        content: '<div type="html" xml:base="/2021/06/12/serial-post-sat-jun-12-19-04-59-eest-2021.html"/>',
         publicationTimestamp: new Date('2021-06-12T19:05:00+03:00'),
         link: new URL('/2021/06/12/serial-post-sat-jun-12-19-04-59-eest-2021.html', baseURL),
       },
       {
         title: 'A new post',
-        content: '',
+        content: '<div type="html" xml:base="/2021/06/12/a-new-post.html"/>',
         publicationTimestamp: new Date('2021-06-12T19:03:00+03:00'),
         link: new URL('/2021/06/12/a-new-post.html', baseURL),
       },
       {
-        title: 'Welcome to Jekyll',
-        content: 'TBD',
+        title: 'Welcome to Jekyll!',
+        content:
+          '\n      <p>You’ll find this post in your <code class="language-plaintext highlighter-rouge">_posts</code> directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run <code class="language-plaintext highlighter-rouge">jekyll serve</code>, which launches a web server and auto-regenerates your site when a file is updated.</p>\n',
         publicationTimestamp: new Date('2021-06-12T18:50:16+03:00'),
         link: new URL('/jekyll/update/2021/06/12/welcome-to-jekyll.html', baseURL),
       },
@@ -37,7 +38,7 @@ describe(parseRssItems.name, () => {
 
     expect(result).to.deep.equal({
       kind: 'ValidRssItems',
-      items: [], // TODO: Replace with expectedItems
+      items: expectedItems,
     });
   });
 
@@ -46,7 +47,7 @@ describe(parseRssItems.name, () => {
       const inputItem: Item = {
         title: 'Post title',
         content: 'Post body',
-        pubDate: new Date().toJSON(),
+        isoDate: new Date().toJSON(),
         link: '/the/path/to/file.html',
       };
 
@@ -55,7 +56,7 @@ describe(parseRssItems.name, () => {
         value: {
           title: inputItem.title!,
           content: inputItem.content!,
-          publicationTimestamp: new Date(inputItem.pubDate!),
+          publicationTimestamp: new Date(inputItem.isoDate!),
           link: new URL(inputItem.link!, baseURL),
         },
       };
@@ -63,33 +64,25 @@ describe(parseRssItems.name, () => {
       expect(buildRssItem(inputItem, baseURL)).to.deep.equal(expectedResult);
     });
 
-    it('returns a ValidRssItem value when input is valid', () => {
+    it('returns a InvalidRssItem value when input is invalid', () => {
       const inputItem: Item = {
         title: 'Post title',
         content: 'Post body',
-        pubDate: new Date().toJSON(),
+        isoDate: new Date().toJSON(),
         link: '/the/path/to/file.html',
       };
 
       const invalid = (reason: string) => ({ kind: 'InvalidRssItem', reason });
+      const build = (itemProps: Partial<Item>): ReturnType<typeof buildRssItem> =>
+        buildRssItem({ ...inputItem, ...itemProps }, baseURL);
 
-      expect(buildRssItem({ ...inputItem, title: undefined }, baseURL)).to.deep.equal(invalid('Post title is missing'));
-      expect(buildRssItem({ ...inputItem, content: undefined }, baseURL)).to.deep.equal(
-        invalid('Post content is missing')
-      );
-      expect(buildRssItem({ ...inputItem, pubDate: undefined }, baseURL)).to.deep.equal(
-        invalid('Post publication timestamp is missing')
-      );
-      expect(buildRssItem({ ...inputItem, pubDate: 'Not a JSON date string' }, baseURL)).to.deep.equal(
+      expect(build({ title: undefined })).to.deep.equal(invalid('Post title is missing'));
+      expect(build({ content: undefined })).to.deep.equal(invalid('Post content is missing'));
+      expect(build({ isoDate: undefined })).to.deep.equal(invalid('Post publication timestamp is missing'));
+      expect(build({ isoDate: 'Not a JSON date string' })).to.deep.equal(
         invalid('Post publication timestamp is an invalid JSON date string')
       );
-      expect(buildRssItem({ ...inputItem, link: undefined }, baseURL)).to.deep.equal(invalid('Post link is missing'));
-
-      // TODO:
-
-      // expect(buildRssItem({ ...inputItem, link: '../../etc/passwd' }, baseURL)).to.deep.equal(
-      //   invalid('Post link is invalid')
-      // );
+      expect(build({ link: undefined })).to.deep.equal(invalid('Post link is missing'));
     });
   });
 });
@@ -111,14 +104,26 @@ interface InvalidRssItems {
   reason: string;
 }
 
-async function parseRssItems(rssResponse: ValidRssResponse): Promise<ValidRssItems | InvalidRssItems> {
+interface RssParseResult {
+  validRssItems: ValidRssItem[];
+  invalidRssItems: InvalidRssItem[];
+  message: string;
+}
+
+/**
+ * TODO: return RssParseResult
+ */
+
+async function parseRssItems(rssResponse: ValidRssResponse): Promise<RssParseResult> {
   const parser = new Parser();
   const feed = await parser.parseString(rssResponse.xml);
   const items = feed.items.map((item) => buildRssItem(item, rssResponse.baseURL));
+  const validItems = items.filter(isValidRssItem).map((i) => i.value);
 
   return {
-    kind: 'ValidRssItems',
-    items: [], // TODO: Replace with `items`
+    validRssItems: [], // TODO
+    invalidRssItems: [], // TODO
+    message: 'TODO', // TODO
   };
 }
 
@@ -127,13 +132,16 @@ interface ValidRssItem {
   value: RssItem;
 }
 
+function isValidRssItem(value: any): value is ValidRssItem {
+  return value.kind === 'ValidRssItem';
+}
+
 interface InvalidRssItem {
   kind: 'InvalidRssItem';
   reason: string;
 }
 
 function buildRssItem(item: Item, baseURL: URL): ValidRssItem | InvalidRssItem {
-  // TODO: What is item.isoDate?
   if (!item.title?.trim()) {
     return {
       kind: 'InvalidRssItem',
@@ -148,14 +156,13 @@ function buildRssItem(item: Item, baseURL: URL): ValidRssItem | InvalidRssItem {
     };
   }
 
-  if (!item.pubDate?.trim()) {
+  if (!item.isoDate?.trim()) {
     return {
       kind: 'InvalidRssItem',
       reason: 'Post publication timestamp is missing',
     };
   }
 
-  let link: URL;
   const linkString = item.link?.trim();
 
   if (!linkString) {
@@ -163,18 +170,10 @@ function buildRssItem(item: Item, baseURL: URL): ValidRssItem | InvalidRssItem {
       kind: 'InvalidRssItem',
       reason: 'Post link is missing',
     };
-    // } else {
-    //   try {
-    //     link = new URL(linkString, baseURL)
-    //   } catch(e) {
-    //     return {
-    //       kind: 'InvalidRssItem',
-    //       reason: 'Post publication timestamp is missing',
-    //     };
-    //   }
   }
 
-  const publicationTimestamp = new Date(item.pubDate?.trim());
+  const link = new URL(linkString, baseURL);
+  const publicationTimestamp = new Date(item.isoDate?.trim());
 
   if (publicationTimestamp.toString() === 'Invalid Date') {
     return {
@@ -188,8 +187,8 @@ function buildRssItem(item: Item, baseURL: URL): ValidRssItem | InvalidRssItem {
     value: {
       title: item.title,
       content: item.content,
-      publicationTimestamp: new Date(item.pubDate),
-      link: new URL(item.link, baseURL),
+      publicationTimestamp: new Date(item.isoDate),
+      link: link,
     },
   };
 }
