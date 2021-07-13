2b85196eec606932b973856aa0f0931a3fde5e63
Vlad GURDIGA <gurdiga@gmail.com>
2021-07-13 13:24:59 +0300

    Add shared/ev

---
+++ src/shared/env.ts
@@ -0,0 +1,28 @@
+type EnvVarName = 'FROM_EMAIL_ADDRESS' | 'SMTP_HOST' | 'SMTP_PORT' | 'SMTP_USER' | 'SMTP_PASSWORD';
+
+export function requireEnvVar(varName: EnvVarName): string {
+  assert(varName in process.env, `Env var is missing: ${varName}`);
+
+  return process.env[varName]!;
+}
+
+export function requireNumericEnvVar(varName: EnvVarName): number {
+  const n = parseInt(requireEnvVar(varName), 10);
+
+  assert(!isNaN(n), `Expected env var ${varName} to be a number, not ${n}`);
+
+  return n;
+}
+
+function assert(precondition: boolean, message: string): void {
+  if (!precondition) {
+    throw new EnvironmentAssertionError(message);
+  }
+}
+
+export class EnvironmentAssertionError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'EnvironmentAssertionError';
+  }
+}
