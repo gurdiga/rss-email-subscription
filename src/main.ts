94c8e4da8b5216a770cc38a82da0037970105596
Vlad GURDIGA <gurdiga@gmail.com>
2021-08-26 10:27:52 +0300

    app: exit when DATA_DIR envar missing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { CronJob } from 'cron';
 import { main as checkRss } from './rss-checking/main';
+import { logError } from './shared/logging';
 
 function main() {
-  const dataDir = process.env.DATA_DIR || 'Empty DATA_DIR envar';
+  const dataDir = process.env.DATA_DIR;
+
+  if (!dataDir) {
+    logError(`DATA_DIR envar missing`);
+    return;
+  }
 
   schedule('*/10 * * * * *', () => checkRss(dataDir));
 }
