7b956c4b5c3a2afa78b4249df4e2c95b48792a90
Vlad GURDIGA <gurdiga@gmail.com>
2021-08-30 11:21:03 +0300

    Housekeeping

--- src/cron.ts
+++ src/cron.ts
@@ -1,10 +1,10 @@
 import { CronJob } from 'cron';
-import { readdir } from 'fs/promises';
+import { readdirSync } from 'fs';
 import path from 'path';
 import { main as checkRss } from './rss-checking/main';
 import { logError, logInfo } from './shared/logging';
 
-async function main() {
+function main() {
   const dataDirRoot = process.env.DATA_DIR_ROOT;
 
   if (!dataDirRoot) {
@@ -12,24 +12,24 @@ async function main() {
     return;
   }
 
-  const dataDirs = (await readdir(dataDirRoot, { withFileTypes: true })).filter((x) => x.isDirectory());
+  const dataDirs = readdirSync(dataDirRoot, { withFileTypes: true }).filter((x) => x.isDirectory());
 
   if (dataDirs.length === 0) {
     logError(`No dataDirs in dataDirRoot`, { dataDirRoot });
     return;
   }
 
-  // TODO: Consider limiting the level of parallelisation if/when needed.
-  dataDirs.forEach(({ name }) => {
+  for (const { name } of dataDirs) {
     const dataDir = path.join(dataDirRoot, name);
     const cronPattern = '*/10 * * * * *'; // TODO: switch to `0 0 * * *` when finished
 
     logInfo(`Scheduling RSS check for ${dataDir}`, { cronPattern });
     schedule(cronPattern, () => checkRss(dataDir));
-  });
+  }
 }
 
 function schedule(cronPattern: string, job: () => void): void {
+  // TODO: Consider limiting the level of parallelisation if/when needed.
   new CronJob(cronPattern, job).start();
 }
 
