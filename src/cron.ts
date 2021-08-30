fe6f169003192395b2dac13b1a3d6a0f01df0263
Vlad GURDIGA <gurdiga@gmail.com>
2021-08-30 10:41:53 +0300

    cron: Schedule RSS checks

--- src/cron.ts
+++ src/cron.ts
@@ -1,16 +1,32 @@
 import { CronJob } from 'cron';
+import { readdir } from 'fs/promises';
+import path from 'path';
 import { main as checkRss } from './rss-checking/main';
-import { logError } from './shared/logging';
+import { logError, logInfo } from './shared/logging';
 
-function main() {
-  const dataDir = process.env.DATA_DIR;
+async function main() {
+  const dataDirRoot = process.env.DATA_DIR_ROOT;
 
-  if (!dataDir) {
-    logError(`DATA_DIR envar missing`);
+  if (!dataDirRoot) {
+    logError(`DATA_DIR_ROOT envar missing`);
     return;
   }
 
-  schedule('*/10 * * * * *', () => checkRss(dataDir));
+  const dataDirs = (await readdir(dataDirRoot, { withFileTypes: true })).filter((x) => x.isDirectory());
+
+  if (dataDirs.length === 0) {
+    logError(`No dataDirs in dataDirRoot`, { dataDirRoot });
+    return;
+  }
+
+  // TODO: Consider limiting the level of parallelisation if/when needed.
+  dataDirs.forEach(({ name }) => {
+    const dataDir = path.join(dataDirRoot, name);
+    const cronPattern = '*/10 * * * * *'; // TODO: switch to `0 0 * * *` when finished
+
+    logInfo(`Scheduling RSS check for ${dataDir}`, { cronPattern });
+    schedule(cronPattern, () => checkRss(dataDir));
+  });
 }
 
 function schedule(cronPattern: string, job: () => void): void {
