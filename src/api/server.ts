fdc29627f4e6fdf05b5f9d0e83dc0bd5af729b77
Vlad GURDIGA <gurdiga@gmail.com>
2021-11-02 08:40:21 +0200

    Housekeeping

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,51 +1,54 @@
-import express from 'express';
+import express, { RequestHandler } from 'express';
 import helmet from 'helmet';
 import { isErr } from '../shared/lang';
 import { logError, logInfo } from '../shared/logging';
 import { unsubscribe } from './unsubscription';
 
-const app = express();
+function main() {
+  const dataDirRoot = process.env.DATA_DIR_ROOT;
 
-const dataDirRoot = process.env.DATA_DIR_ROOT;
+  if (!dataDirRoot) {
+    logError(`DATA_DIR_ROOT envar missing`);
+    process.exit(1);
+  }
 
-if (!dataDirRoot) {
-  logError(`DATA_DIR_ROOT envar missing`);
-  process.exit(1);
+  const port = 3000;
+  const app = express();
+
+  app.use(helmet());
+  app.use(express.urlencoded({ extended: true }));
+  app.post('/unsubscribe', unsubscriptionHandler(dataDirRoot));
+
+  app.listen(port, () => {
+    logInfo(`Running on http://0.0.0.0:${port}`, { dataDirRoot });
+  });
 }
 
-app.use(helmet());
-app.use(express.urlencoded({ extended: true }));
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+function unsubscriptionHandler(dataDirRoot: string): RequestHandler {
+  return (req, res) => {
+    const { body } = req;
 
-app.post('/unsubscribe', (req, res) => {
-  const { body } = req;
+    logInfo('Unsubscription request', { body, dataDirRoot });
 
-  logInfo('Unsubscription request', { body, dataDirRoot });
+    const { id } = body;
+    const result = unsubscribe(id, dataDirRoot);
 
-  const { id } = body;
-  const result = unsubscribe(id, dataDirRoot);
+    if (isErr(result)) {
+      logError('Unsubscription request failed', { body, reason: result.reason });
+      res.sendStatus(500);
+      return;
+    }
 
-  if (isErr(result)) {
-    logError('Unsubscription request failed', { body, reason: result.reason });
-    res.sendStatus(500);
-    return;
-  }
+    if (result.kind === 'NotFound') {
+      logInfo('Not found', { id });
+      res.sendStatus(404);
+      return;
+    }
 
-  if (result.kind === 'NotFound') {
-    logInfo('Not found', { id });
-    res.sendStatus(404);
-    return;
-  }
+    logInfo('Unsubscription request succeded', { body });
 
-  logInfo('Unsubscription request succeded', { body });
+    res.sendStatus(200);
+  };
+}
 
-  res.sendStatus(200);
-});
-
-const port = 3000;
-
-app.listen(port, () => {
-  logInfo(`Running on http://0.0.0.0:${port}`, { dataDirRoot });
-});
+main();
