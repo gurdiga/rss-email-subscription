f270be942c077d6f678fb5293e4b0b555d5c87fe
Vlad GURDIGA <gurdiga@gmail.com>
2022-09-07 14:13:06 +0300

    API: more sketching /create-account

--- src/api/create-account.ts
+++ src/api/create-account.ts
@@ -1,4 +1,6 @@
-import { makeAppError } from '../shared/api-response';
+import { EmailAddress } from '../app/email-sending/emails';
+import { AppError, InputError, makeAppError, makeInputError } from '../shared/api-response';
+import { makeCustomLoggers } from '../shared/logging';
 import { AppRequestHandler } from './shared';
 
 export const createAccount: AppRequestHandler = async function oneClickUnsubscribe(
@@ -7,7 +9,35 @@ export const createAccount: AppRequestHandler = async function oneClickUnsubscri
   _reqParams,
   dataDirRoot
 ) {
-  console.log({ reqId, reqBody, _reqParams, dataDirRoot });
+  const { plan, email, password } = reqBody;
+  const inputProcessingResult = processInput({ plan, email, password });
+
+  if (inputProcessingResult.kind !== 'ProcessedInput') {
+    return inputProcessingResult;
+  }
+
+  const { logWarning, logError } = makeCustomLoggers({ plan, email, module: createAccount.name });
+
+  console.log({ plan, email, password }, { reqId, dataDirRoot, logWarning, logError });
 
   return makeAppError('Not implemented');
 };
+
+interface Input {
+  plan: string;
+  email: string;
+  password: string;
+}
+
+interface ProcessedInput {
+  kind: 'ProcessedInput';
+  plan: PlanId;
+  email: EmailAddress;
+  password: string; // require min len of 16
+}
+
+type PlanId = 'minimal' | 'standard' | 'sde';
+
+function processInput({}: Input): ProcessedInput | InputError | AppError {
+  return makeInputError('Not implemented');
+}
