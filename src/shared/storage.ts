af6615f0de54ee6222bca1657bb827f25e931184
Vlad GURDIGA <gurdiga@gmail.com>
2022-09-19 11:36:09 +0300

    storage: fix storeItem

--- src/shared/storage.ts
+++ src/shared/storage.ts
@@ -1,9 +1,18 @@
-import { writeFile } from 'node:fs/promises';
+import { writeFile, mkdir } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileExists } from './io';
 
 export async function storeItem(
   key: string,
   value: any,
   dataDirRoot: string = process.env['DATA_DIR_ROOT']!
 ): Promise<void> {
-  return writeFile(`${dataDirRoot}/${key}`, JSON.stringify(value));
+  const filePath = join(dataDirRoot, key);
+  const dirPath = dirname(filePath);
+
+  if (!fileExists(dirPath)) {
+    await mkdir(dirPath, { recursive: true });
+  }
+
+  return writeFile(filePath, JSON.stringify(value));
 }
