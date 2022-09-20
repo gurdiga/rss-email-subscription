a13182254166c6fc1195b73f80ff8d447a01b32a
Vlad GURDIGA <gurdiga@gmail.com>
2022-09-20 11:58:48 +0300

    storage: loadItem & makeStorage

--- src/shared/storage.spec.ts
+++ src/shared/storage.spec.ts
@@ -1,51 +1,71 @@
 import { expect } from 'chai';
-import { FileExistsFn, MkdirpFn, WriteFileFn } from './io';
+import { FileExistsFn, MkdirpFn, ReadFileFn, WriteFileFn } from './io';
 import { makeErr } from './lang';
-import { storeItem } from './storage';
+import { makeStorage } from './storage';
 import { makeStub } from './test-utils';
 
-describe(storeItem.name, () => {
-  const key = '/path/destination.json';
-  const value = { number: 1, string: 'string', date: new Date() };
+describe(makeStorage.name, () => {
   const dataDirRoot = '/data';
+  const { loadItem, storeItem } = makeStorage(dataDirRoot);
 
-  const fileExistsFn = makeStub<FileExistsFn>(() => false);
-  const writeFileFn = makeStub<WriteFileFn>();
-  const mkdirpFn = makeStub<MkdirpFn>();
+  describe(storeItem.name, () => {
+    const key = '/path/destination.json';
+    const value = { number: 1, string: 'string', date: new Date() };
 
-  it('stores the given value JSONified in the given file', () => {
-    const result = storeItem(key, value, dataDirRoot, mkdirpFn, writeFileFn, fileExistsFn);
+    const fileExistsFn = makeStub<FileExistsFn>(() => false);
+    const writeFileFn = makeStub<WriteFileFn>();
+    const mkdirpFn = makeStub<MkdirpFn>();
 
-    expect(mkdirpFn.calls[0]).deep.equal(['/data/path'], 'creates the necessary directory structure');
-    expect(writeFileFn.calls[0]).deep.equal(
-      ['/data/path/destination.json', JSON.stringify(value)],
-      'stores data in the given file'
-    );
-    expect(result).to.be.true;
+    it('stores the given value JSONified in the given file', () => {
+      const result = storeItem(key, value, mkdirpFn, writeFileFn, fileExistsFn);
+
+      expect(mkdirpFn.calls[0]).deep.equal(['/data/path'], 'creates the necessary directory structure');
+      expect(writeFileFn.calls[0]).deep.equal(
+        ['/data/path/destination.json', JSON.stringify(value)],
+        'stores data in the given file'
+      );
+      expect(result).to.be.true;
+    });
+
+    it('returns an Err value when can’t create directory structure', () => {
+      const errorMessage = 'Can’t create directory structure!!';
+      const mkdirpFn = makeStub<MkdirpFn>(() => {
+        throw new Error(errorMessage);
+      });
+      const result = storeItem(key, value, mkdirpFn, writeFileFn, fileExistsFn);
+
+      expect(mkdirpFn.calls[0]).deep.equal(['/data/path'], 'tries to create the necessary directory structure');
+      expect(result).to.deep.equal(makeErr(errorMessage));
+    });
+
+    it('returns an Err value when can’t write file', () => {
+      const errorMessage = 'Can’t write file!!';
+      const writeFileFn = makeStub<WriteFileFn>(() => {
+        throw new Error(errorMessage);
+      });
+      const result = storeItem(key, value, mkdirpFn, writeFileFn, fileExistsFn);
+
+      expect(writeFileFn.calls[0]).deep.equal(
+        ['/data/path/destination.json', JSON.stringify(value)],
+        'stores data in the given file'
+      );
+      expect(result).to.deep.equal(makeErr(errorMessage));
+    });
   });
 
-  it('returns an Err value when can’t create directory structure', () => {
-    const errorMessage = 'Can’t create directory structure!!';
-    const mkdirpFn = makeStub<MkdirpFn>(() => {
-      throw new Error(errorMessage);
+  describe(loadItem.name, () => {
+    const key = '/path/destination.json';
+    const value = { number: 1, string: 'string', date: new Date() };
+
+    const readFileFn = makeStub<ReadFileFn>(() => JSON.stringify(value));
+
+    it('loads a JSON blob from the given file', () => {
+      const result = loadItem(key, readFileFn);
+
+      expect(result).to.deep.equal(value);
     });
-    const result = storeItem(key, value, dataDirRoot, mkdirpFn, writeFileFn, fileExistsFn);
 
-    expect(mkdirpFn.calls[0]).deep.equal(['/data/path'], 'tries to create the necessary directory structure');
-    expect(result).to.deep.equal(makeErr(errorMessage));
-  });
-
-  it('returns an Err value when can’t write file', () => {
-    const errorMessage = 'Can’t write file!!';
-    const writeFileFn = makeStub<WriteFileFn>(() => {
-      throw new Error(errorMessage);
-    });
-    const result = storeItem(key, value, dataDirRoot, mkdirpFn, writeFileFn, fileExistsFn);
-
-    expect(writeFileFn.calls[0]).deep.equal(
-      ['/data/path/destination.json', JSON.stringify(value)],
-      'stores data in the given file'
-    );
-    expect(result).to.deep.equal(makeErr(errorMessage));
+    it('returns an Err value when can’t read settings.json file');
+    it('returns an Err value when can’t parse settings.json contents');
   });
 });
--- src/shared/storage.ts
+++ src/shared/storage.ts
@@ -1,31 +1,73 @@
 import { dirname, join } from 'node:path';
-import { fileExists, FileExistsFn, mkdirp, MkdirpFn, writeFile, WriteFileFn } from './io';
-import { getErrorMessage, makeErr, Result } from './lang';
+import { fileExists, FileExistsFn, mkdirp, MkdirpFn, readFile, ReadFileFn, writeFile, WriteFileFn } from './io';
+import { makeErr, Result } from './lang';
 
-export function storeItem(
-  key: string,
-  value: any,
-  dataDirRoot: string = process.env['DATA_DIR_ROOT']!,
-  mkdirpFn: MkdirpFn = mkdirp,
-  writeFileFn: WriteFileFn = writeFile,
-  fileExistsFn: FileExistsFn = fileExists
-): Result<true> {
-  const filePath = join(dataDirRoot, key);
-  const dirPath = dirname(filePath);
+export interface AppStorage {
+  storeItem: StoreItemFn;
+  loadItem: LoadItemFn;
+}
 
-  if (!fileExistsFn(dirPath)) {
-    try {
-      mkdirpFn(dirPath);
-    } catch (error) {
-      return makeErr(getErrorMessage(error));
+export function makeStorage(dataDirRoot: string): AppStorage {
+  return {
+    storeItem: makeStoreItemFn(dataDirRoot),
+    loadItem: makeLoadItemFn(dataDirRoot),
+  };
+}
+
+type StorageKey = string; // Something like this: '/accounts/219812984/account.json'
+type StorageValue = any; // Will get JSONified and stored in the file. TODO: Maybe constrain the type
+
+type StoreItemFn = (
+  key: StorageKey,
+  value: StorageValue,
+  mkdirpFn?: MkdirpFn,
+  writeFileFn?: WriteFileFn,
+  fileExistsFn?: FileExistsFn
+) => Result<true>;
+
+function makeStoreItemFn(dataDirRoot: string): StoreItemFn {
+  return <StoreItemFn>(
+    function storeItem(key, value, mkdirpFn = mkdirp, writeFileFn = writeFile, fileExistsFn = fileExists) {
+      const filePath = join(dataDirRoot, key);
+      const dirPath = dirname(filePath);
+
+      if (!fileExistsFn(dirPath)) {
+        try {
+          mkdirpFn(dirPath);
+        } catch (error) {
+          return makeErr(error);
+        }
+      }
+
+      try {
+        writeFileFn(filePath, JSON.stringify(value));
+      } catch (error) {
+        return makeErr(error);
+      }
+
+      return true;
     }
-  }
+  );
+}
 
-  try {
-    writeFileFn(filePath, JSON.stringify(value));
-  } catch (error) {
-    return makeErr(getErrorMessage(error));
-  }
+type LoadItemFn = (key: StorageKey, readFileFn: ReadFileFn) => StorageValue;
 
-  return true;
+function makeLoadItemFn(dataDirRoot: string): LoadItemFn {
+  const dateRegExp = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/;
+
+  return <LoadItemFn>function loadItem(key, readFileFn = readFile) {
+    const filePath = join(dataDirRoot, key);
+
+    try {
+      const json = readFileFn(filePath);
+
+      try {
+        return JSON.parse(json, (_k, v) => (typeof v === 'string' && dateRegExp.test(v) ? new Date(v) : v));
+      } catch (error) {
+        return makeErr(error);
+      }
+    } catch (error) {
+      return makeErr(error);
+    }
+  };
 }
