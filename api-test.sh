2758fabca05d4999175a392e19d34990010dfb81
Vlad GURDIGA <gurdiga@gmail.com>
2022-09-24 17:54:30 +0300

    api-test: housekeeping

--- api-test.sh
+++ api-test.sh
@@ -9,9 +9,14 @@ EMAIL_HASH=ea7f63853ce24fe12963ea07fd5f363dc2292f882f268c1b8f605076c672b4e9
 FEED_ID=gurdiga
 EMAIL_DATA_FILE=$DATA_DIR_ROOT/$FEED_ID/emails.json
 
+ACCOUNT_PLAN=standard
+ACCOUNT_EMAIL=blogger@test.com
+ACCOUNT_PASSWORD=A-long-S3cre7-password
+
 function main {
-	create_account
-	create_account_verify
+	create_account $ACCOUNT_PLAN $ACCOUNT_EMAIL $ACCOUNT_PASSWORD
+	create_account_verify $ACCOUNT_PLAN $ACCOUNT_EMAIL
+	remove_accounts $ACCOUNT_EMAIL
 	unsubscribe
 	unsubscribe_verify
 	subscribe
@@ -47,11 +52,12 @@ function get_headers {
 	curl -ks --head $BASE_URL"$@"
 }
 
-ACCOUNT_PLAN=standard
-ACCOUNT_EMAIL=blogger@test.com
-
 function create_account {
-	if post /create-account -d plan=$ACCOUNT_PLAN -d email=$ACCOUNT_EMAIL -d password=A-long-S3cre7-password; then
+	local account_plan=$1
+	local account_email=$2
+	local account_password=$3
+
+	if post /create-account -d plan="$account_plan" -d email="$account_email" -d password="$account_password"; then
 		print_success
 	else
 		print_failure "POST /create-account failed: exit code $?"
@@ -59,35 +65,47 @@ function create_account {
 }
 
 function create_account_verify {
-	account_file=$(
-		find "$DATA_DIR_ROOT/accounts" -name account.json |
-			while read -r account_file_path; do
-				if grep "\"email\":\"$ACCOUNT_EMAIL\"" "$account_file_path" >/dev/null; then
-					echo "$account_file_path"
-				fi
-			done
-	)
+	local account_plan=${1:?}
+	local account_email=${2:?}
 
-	file_count=$(wc -l <<<"$account_file")
+	local account_file && account_file=$(find_account_files_by_email "${account_email}")
+	local file_count && file_count=$(wc -l <<<"$account_file")
 
 	if [ "$file_count" -eq "0" ]; then
 		print_failure "Account file not found"
 	elif [ "$file_count" -gt "1" ]; then
-		print_failure "Found more than one account files with for $ACCOUNT_EMAIL"
+		print_failure "Found more than one account files with for $account_email"
 	fi
 
-	snapshot='{"plan":"standard","email":"blogger@test.com","passwordHash":".+"}'
+	snapshot='{"plan":"'$account_plan'","email":"'$account_email'","passwordHash":".+"}'
 
 	if grep -E "$snapshot" "$account_file" >/dev/null; then
 		print_success
 	else
-		echo -e "Expected: '$snapshot'\n"
-		echo -e "Actual: '$(cat "$account_file")'\n"
+		echo -e "$(yellow Expected:) $snapshot\n"
+		echo -e "$(yellow Actual:) $(cat "$account_file")\n"
 
 		print_failure 'Account file contents does not match'
 	fi
+}
 
-	rm -vfr "$(dirname "$account_file")"
+function remove_accounts {
+	local account_email=${1:?}
+
+	for account_file in $(find_account_files_by_email "${account_email}"); do
+		rm -vfr "$(dirname "$account_file")"
+	done
+}
+
+function find_account_files_by_email {
+	local account_email=${1:?}
+
+	find "$DATA_DIR_ROOT/accounts" -name account.json |
+		while read -r account_file_path; do
+			if grep "\"email\":\"$account_email\"" "$account_file_path" >/dev/null; then
+				echo "$account_file_path"
+			fi
+		done
 }
 
 function subscribe {
@@ -199,27 +217,26 @@ function verify_has_cors_enabled {
 	assert_header /subscribe 'access-control-allow-origin: *'
 }
 
-RED="\e[31m"
-GREEN="\e[32m"
-YELLOW="\e[33m"
-ENDCOLOR="\e[0m"
-
 function print_success {
-	printf "\n${GREEN}%s${ENDCOLOR}\n\n" OK
+	echo -e "\n$(green OK)\n"
 }
 
 function print_failure {
 	local caller_name="${FUNCNAME[1]}"
-	local prefix="${RED}FAILURE in $caller_name${ENDCOLOR}"
+	local prefix && prefix=$(red FAILURE in "$caller_name")
 
 	if test -z "$@"; then
 		# shellcheck disable=SC2059
 		printf "$prefix\n\n"
 	else
-		printf "\n$prefix: ${YELLOW}%s${ENDCOLOR}\n\n" "$@"
+		echo -e "\n$prefix: $(yellow "$@"))\n\n"
 	fi
 
 	exit 1
 }
+
+function red { printf "\e[31m%s\e[0m" "$@"; }
+function green { printf "\e[32m%s\e[0m" "$@"; }
+function yellow { printf "\e[33m%s\e[0m" "$@"; }
 
 main
