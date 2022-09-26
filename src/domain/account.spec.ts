8349b55d2525790230fa9484f4c06c2fded11103
Vlad GURDIGA <gurdiga@gmail.com>
2022-09-26 14:26:18 +0300

    loadAccount: unit tests for unhappy paths

--- src/domain/account.spec.ts
+++ src/domain/account.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import { EmailAddress, makeEmailAddress } from '../app/email-sending/emails';
+import { makeErr } from '../shared/lang';
 import { makeStorageStub, Stub } from '../shared/test-utils';
 import { AccountData, loadAccount } from './account';
 import { HashedPassword, hashedPasswordLength, makeHashedPassword } from './hashed-password';
@@ -21,5 +22,52 @@ describe(loadAccount.name, () => {
       hashedPassword: makeHashedPassword(accountData.hashedPassword) as HashedPassword,
       plan: accountData.plan,
     });
+  });
+
+  it('returns an Err value when storage fails', () => {
+    const storage = makeStorageStub({ loadItem: () => makeErr('Bad disc sector!') });
+    const result = loadAccount(storage, 123);
+
+    expect(result).to.deep.equal(makeErr('Can’t load account data: Bad disc sector!'));
+  });
+
+  it('returns an Err value when stored email is invalid', () => {
+    const accountData: AccountData = {
+      plan: 'sde',
+      email: 'not-an-email-really',
+      hashedPassword: 'x'.repeat(hashedPasswordLength),
+    };
+    const storage = makeStorageStub({ loadItem: () => accountData });
+    const result = loadAccount(storage, 123);
+
+    expect(result).to.deep.equal(
+      makeErr('Invalid email while loading account 123: Syntactically invalid email: "not-an-email-really"')
+    );
+  });
+
+  it('returns an Err value when stored plan ID is unrecognized', () => {
+    const accountData: AccountData = {
+      plan: 'magic',
+      email: 'test@test.com',
+      hashedPassword: 'x'.repeat(hashedPasswordLength),
+    };
+    const storage = makeStorageStub({ loadItem: () => accountData });
+    const result = loadAccount(storage, 123);
+
+    expect(result).to.deep.equal(makeErr('Invalid plan ID while loading account 123: Unknown plan ID: magic'));
+  });
+
+  it('returns an Err value when stored hashed password is invalid', () => {
+    const accountData: AccountData = {
+      plan: 'sde',
+      email: 'test@test.com',
+      hashedPassword: 'la-la-la',
+    };
+    const storage = makeStorageStub({ loadItem: () => accountData });
+    const result = loadAccount(storage, 123);
+
+    expect(result).to.deep.equal(
+      makeErr('Invalid hashed password while loading account 123: Invalid hashed password length: 8')
+    );
   });
 });
