be3601c5cb9a65e4978725cda55a05f0e2751fe3
Vlad GURDIGA <gurdiga@gmail.com>
2023-02-16 18:06:30 +0200

    web-ui: no ID in manage-feed
    Because the ID is the prefix of the Email, and so it’s not useful as
    additional information. Moreover, it makes the system more complex for
    the non-techy user.

--- src/web-ui/manage-feed.spec.ts
+++ src/web-ui/manage-feed.spec.ts
@@ -20,7 +20,6 @@ describe(makeUiData.name, () => {
 
     expect(result).to.deep.equal(<UiData>{
       feedAttributes: [
-        { label: 'Feed ID:', value: 'just-add-light', name: 'id' },
         { label: 'Name:', value: 'Just Add Light', name: 'displayName' },
         { label: 'URL:', value: 'https://test.com/just-add-light/feed.xml', name: 'url' },
         { label: 'Email:', value: 'just-add-light@test.com', name: 'email' },
