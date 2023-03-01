4f21642b4645fbed09e8041c1474d51da3e965ae
Vlad GURDIGA <gurdiga@gmail.com>
2023-03-01 18:26:31 +0200

    web-ui: navbar works

--- src/web-ui/navbar.ts
+++ src/web-ui/navbar.ts
@@ -1,20 +1,38 @@
 import { navbarCookieName } from '../api/app-cookie';
+import { isErr } from '../shared/lang';
 import { si } from '../shared/string-utils';
+import { displayInitError, hideElement, requireUiElements, unhideElement } from './shared';
+
+interface RequiredUiElements {
+  publicNav: HTMLElement;
+  privateNav: HTMLElement;
+}
 
 function main() {
-  const x = getCookieByName(navbarCookieName);
-  const test = getCookieByName('test');
+  const navbarCookie = getCookieByName(navbarCookieName);
 
-  console.log('Hello navbar', { x, test });
+  const uiElements = requireUiElements<RequiredUiElements>({
+    publicNav: '#right-navbar-nav-public',
+    privateNav: '#right-navbar-nav-private',
+  });
+
+  if (isErr(uiElements)) {
+    displayInitError(uiElements.reason);
+    return;
+  }
+
+  if (navbarCookie === 'true') {
+    unhideElement(uiElements.privateNav);
+    hideElement(uiElements.publicNav);
+  } else {
+    unhideElement(uiElements.publicNav);
+    hideElement(uiElements.privateNav);
+  }
 }
 
 export function getCookieByName(name: string, documentCookie = document.cookie): string {
   const pairs = documentCookie.split('; ');
-  const pair = pairs.find((x) => x.startsWith(si`${name}=`));
-
-  if (!pair) {
-    return '';
-  }
+  const pair = pairs.find((x) => x.startsWith(si`${name}=`)) || '';
 
   const encodedValue = pair.split('=')[1] || '';
   const value = decodeURIComponent(encodedValue);
