85b914b29e2524e9b4ec700a680384cccb3169c4
Vlad GURDIGA <gurdiga@gmail.com>
2023-03-02 11:14:10 +0200

    web-ui: navbar sign out link

--- src/web-ui/navbar.ts
+++ src/web-ui/navbar.ts
@@ -1,11 +1,12 @@
 import { navbarCookieName } from '../api/app-cookie';
-import { isErr } from '../shared/lang';
+import { asyncAttempt, isErr } from '../shared/lang';
 import { si } from '../shared/string-utils';
-import { displayInitError, hideElement, requireUiElements, unhideElement } from './shared';
+import { displayInitError, hideElement, HttpMethod, requireUiElements, sendApiRequest, unhideElement } from './shared';
 
 interface RequiredUiElements {
   publicNav: HTMLElement;
   privateNav: HTMLElement;
+  signOutLink: HTMLAnchorElement;
 }
 
 function main() {
@@ -14,6 +15,7 @@ function main() {
   const uiElements = requireUiElements<RequiredUiElements>({
     publicNav: '#right-navbar-nav-public',
     privateNav: '#right-navbar-nav-private',
+    signOutLink: '#sign-out-link',
   });
 
   if (isErr(uiElements)) {
@@ -22,12 +24,36 @@ function main() {
   }
 
   if (navbarCookie === 'true') {
-    unhideElement(uiElements.privateNav);
-    hideElement(uiElements.publicNav);
+    displayPrivateNavbar(uiElements);
   } else {
-    unhideElement(uiElements.publicNav);
-    hideElement(uiElements.privateNav);
+    displayPublicNavbar(uiElements);
   }
+}
+
+function displayPublicNavbar(uiElements: RequiredUiElements): void {
+  unhideElement(uiElements.publicNav);
+  hideElement(uiElements.privateNav);
+}
+
+function displayPrivateNavbar(uiElements: RequiredUiElements): void {
+  const { privateNav, publicNav, signOutLink } = uiElements;
+
+  unhideElement(privateNav);
+  hideElement(publicNav);
+
+  signOutLink.addEventListener('click', async (e: Event) => {
+    e.preventDefault();
+
+    const response = await asyncAttempt(() => sendApiRequest('/deauthentication', HttpMethod.POST));
+
+    if (isErr(response)) {
+      signOutLink.textContent = 'Failed!';
+      signOutLink.classList.add('text-danger');
+      return;
+    }
+
+    location.href = signOutLink.href;
+  });
 }
 
 export function getCookieByName(name: string, documentCookie = document.cookie): string {
