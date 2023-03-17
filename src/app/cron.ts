efe11534a0263ac7707dba7782d43a444841c049
Vlad GURDIGA <gurdiga@gmail.com>
2023-03-17 12:59:02 +0200

    cron: single job
    Rethinking cron from job-per-feed to single-job. The single job will
    run every hour and check all the feeds one-by-one.
    
    The HUP signal will check the feeds now.

--- src/app/cron.ts
+++ src/app/cron.ts
@@ -1,17 +1,17 @@
-import { CronJob } from 'cron';
-import { checkRss } from '../app/rss-checking';
-import { sendEmails } from '../app/email-sending';
-import { makeCustomLoggers } from '../shared/logging';
-import { loadFeedsByAccountId } from '../domain/feed-storage';
-import { isErr } from '../shared/lang';
-import { AppStorage, makeStorage } from '../domain/storage';
-import { requireEnv } from '../shared/env';
+import { CronCommand, CronJob } from 'cron';
 import { AppEnv } from '../api/init-app';
+import { sendEmails } from '../app/email-sending';
+import { checkRss } from '../app/rss-checking';
 import { makeAccountId } from '../domain/account';
 import { accountsStorageKey } from '../domain/account-storage';
-import { si } from '../shared/string-utils';
-import { isEmpty, isNotEmpty } from '../shared/array-utils';
 import { FeedStatus } from '../domain/feed';
+import { loadFeedsByAccountId } from '../domain/feed-storage';
+import { makeStorage } from '../domain/storage';
+import { isEmpty, isNotEmpty } from '../shared/array-utils';
+import { requireEnv } from '../shared/env';
+import { AnyAsyncFunction, isErr } from '../shared/lang';
+import { makeCustomLoggers } from '../shared/logging';
+import { si } from '../shared/string-utils';
 
 function main() {
   const { logError, logInfo, logWarning } = makeCustomLoggers({ module: 'cron' });
@@ -26,97 +26,103 @@ function main() {
   }
 
   const dataDirRoot = env.DATA_DIR_ROOT;
-  const storage = makeStorage(dataDirRoot);
-  let cronJobs = scheduleFeedChecks(env, storage);
+  const feedCheckingJob = startJob('0 * * * *', () => checkFeeds(dataDirRoot));
+  const errorReportingCheckJob = startJob('0 0 * * *', () => logError('Just checking error reporting'));
 
   process.on('SIGHUP', () => {
-    logWarning('Received SIGUP. Will reload.');
-
-    cronJobs.forEach((j) => j.stop());
-    cronJobs = scheduleFeedChecks(env, storage);
+    logWarning('Received SIGUP. Will check feeds now.');
+    checkFeeds(dataDirRoot);
   });
 
   process.on('SIGTERM', () => {
-    logWarning('Received SIGTERM. Will stop all the cron jobs and exit.');
-    [...cronJobs, errorReportingCheck].forEach((j) => j.stop());
+    logWarning('Received SIGTERM. Will stop the cron job and exit.');
+    feedCheckingJob.stop();
+    errorReportingCheckJob.stop();
   });
-
-  const errorReportingCheck = scheduleErrorReportingCheck();
 }
 
-function scheduleFeedChecks(env: AppEnv, storage: AppStorage): CronJob[] {
-  const { logError, logInfo } = makeCustomLoggers({ module: 'cron' });
-  let accountDirs = storage.listSubdirectories(accountsStorageKey);
+async function checkFeeds(dataDirRoot: string): Promise<void> {
+  const logData = { module: 'cron' };
+  const loggers = makeCustomLoggers(logData);
 
-  if (isErr(accountDirs)) {
-    logError('Failed to list account subdirectories', { reason: accountDirs.reason });
-    process.exit(1);
-  }
+  logDuration('Feed checking', loggers, async () => {
+    const { logError, logInfo } = loggers;
+    const storage = makeStorage(dataDirRoot);
+    const accountDirs = storage.listSubdirectories(accountsStorageKey);
 
-  if (accountDirs.length === 0) {
-    logError('No accountDirs in dataDirRoot', { dataDirRoot: env.DATA_DIR_ROOT });
-    process.exit(1);
-  }
-
-  const cronJobs: CronJob[] = [];
-
-  for (const dirName of accountDirs) {
-    const accountId = makeAccountId(dirName);
-
-    if (isErr(accountId)) {
-      logError('Invalid accountId', { dirName, reason: accountId.reason });
-      continue;
+    if (isErr(accountDirs)) {
+      logError('Failed to list account subdirectories', { reason: accountDirs.reason });
+      process.exit(1);
     }
 
-    const feedsByAccountId = loadFeedsByAccountId(accountId, storage);
-
-    if (isErr(feedsByAccountId)) {
-      logError(si`Failed to ${loadFeedsByAccountId.name}`, { dirName, reason: feedsByAccountId.reason });
-      continue;
+    if (isEmpty(accountDirs)) {
+      logError('No accounts?!', { dataDirRoot });
+      process.exit(1);
     }
 
-    if (isNotEmpty(feedsByAccountId.errs)) {
-      logError(si`Errors on ${loadFeedsByAccountId.name}`, {
-        dirName,
-        errs: feedsByAccountId.errs.map((x) => x.reason),
-      });
+    for (const dirName of accountDirs) {
+      const accountId = makeAccountId(dirName);
+
+      if (isErr(accountId)) {
+        logError(si`Failed to ${makeAccountId.name}`, { input: dirName, reason: accountId.reason });
+        continue;
+      }
+
+      const feedsByAccountId = loadFeedsByAccountId(accountId, storage);
+
+      if (isErr(feedsByAccountId)) {
+        logError(si`Failed to ${loadFeedsByAccountId.name}`, {
+          accountId: accountId.value,
+          reason: feedsByAccountId.reason,
+        });
+        continue;
+      }
+
+      if (isNotEmpty(feedsByAccountId.errs)) {
+        const errs = feedsByAccountId.errs.map((x) => x.reason);
+        logError(si`Errors on ${loadFeedsByAccountId.name}`, { accountId: accountId.value, errs });
+      }
+
+      if (isEmpty(feedsByAccountId.validFeeds)) {
+        logInfo('No feeds for account', { accountId: accountId.value });
+      }
+
+      const approvedFeeds = feedsByAccountId.validFeeds.filter((x) => x.status === FeedStatus.Approved && !x.isDeleted);
+      const feedCount = approvedFeeds.length;
+
+      logInfo(si`Checking ${feedCount} feeds`, { accountId: accountId.value });
+
+      for (const feed of approvedFeeds) {
+        const logLabel = si`${feed.displayName} (${feed.id.value})`;
+
+        await logDuration(si`RSS checking: ${logLabel}`, loggers, () => checkRss(accountId, feed, storage));
+        await logDuration(si`Email sending: ${logLabel}`, loggers, () => sendEmails(accountId, feed, storage));
+      }
     }
-
-    if (isEmpty(feedsByAccountId.validFeeds)) {
-      logInfo('No feeds for account', { dirName });
-    }
-
-    const approvedFeeds = feedsByAccountId.validFeeds.filter((x) => x.status === FeedStatus.Approved);
-
-    for (const feed of approvedFeeds) {
-      logInfo('Scheduling feed check', { feed });
-
-      cronJobs.push(
-        new CronJob(feed.cronPattern.value, async () => {
-          await checkRss(accountId, feed, storage);
-          await sendEmails(accountId, feed, storage);
-        })
-      );
-    }
-  }
-
-  cronJobs.forEach((j) => j.start());
-
-  return cronJobs;
+  });
 }
 
-function scheduleErrorReportingCheck(): CronJob {
-  const { logError, logWarning } = makeCustomLoggers({ module: 'error-reporting-check' });
+async function logDuration(
+  label: string,
+  { logInfo }: ReturnType<typeof makeCustomLoggers>,
+  f: AnyAsyncFunction
+): Promise<void> {
+  const startTimestamp = new Date();
 
-  logWarning('Starting a daily job to check error reporting');
+  logInfo(si`Started ${label}`);
+  await f();
 
-  const job = new CronJob('0 0 * * *', async () => {
-    logError('Just checking error reporting');
-  });
+  const endTimestamp = new Date();
+  const durationMs = endTimestamp.valueOf() - startTimestamp.valueOf();
 
-  job.start();
+  logInfo(si`Finished ${label}`, { durationMs });
+}
 
-  return job;
+function startJob(cronTime: string, onTick: CronCommand): CronJob {
+  const startNow = true;
+  const onComplete = null;
+
+  return new CronJob(cronTime, onTick, onComplete, startNow);
 }
 
 main();
