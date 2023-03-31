34a9064d9ab2a8a7a3f44ecf5bf6da8e49e6f6a9
Vlad GURDIGA <gurdiga@gmail.com>
2023-03-31 18:15:30 +0300

    web-ui/pricing: build card links

--- src/web-ui/pricing.ts
+++ src/web-ui/pricing.ts
@@ -1,4 +1,5 @@
-import { Plans } from '../domain/plan';
+import { makePagePathWithParams, PagePath } from '../domain/page-path';
+import { PlanId, Plans } from '../domain/plan';
 import { isErr } from '../shared/lang';
 import { displayInitError, fillUiElements, requireUiElements } from './shared';
 
@@ -7,11 +8,14 @@ async function main() {
     freeTitle: '#free-title',
     freeMaxEmailsPerMonth: '#free-max-emails-per-month',
     freeMaxEmailsPerDay: '#free-max-emails-per-day',
+    freeLink: '#free-link',
     ppuTitle: '#ppu-title',
     ppuMaxEmailsPerDay: '#ppu-max-emails-per-day',
     ppuMaxEmailsPerMonth: '#ppu-max-emails-per-month',
     ppuPricePerEmailCents: '#ppu-price-per-email-cents',
+    ppuLink: '#ppu-link',
     sdeTitle: '#sde-title',
+    sdeLink: '#sde-link',
   });
 
   if (isErr(uiElements)) {
@@ -38,6 +42,12 @@ async function main() {
       value: formatNumber(Plans.free.maxEmailsPerMonth),
     },
     {
+      element: uiElements.freeLink,
+      propName: 'href',
+      value: makePagePathWithParams(PagePath.registration, { plan: PlanId.Free }),
+    },
+
+    {
       element: uiElements.ppuTitle,
       propName: 'textContent',
       value: Plans.ppu.title,
@@ -58,9 +68,19 @@ async function main() {
       value: formatNumber(Plans.ppu.maxEmailsPerMonth),
     },
     {
+      element: uiElements.ppuLink,
+      propName: 'href',
+      value: makePagePathWithParams(PagePath.registration, { plan: PlanId.PayPerUse }),
+    },
+    {
       element: uiElements.sdeTitle,
       propName: 'textContent',
       value: Plans.sde.title,
+    },
+    {
+      element: uiElements.sdeLink,
+      propName: 'href',
+      value: makePagePathWithParams(PagePath.registration, { plan: PlanId.SDE }),
     },
   ]);
 }
@@ -69,11 +89,14 @@ interface RequiredUiElements {
   freeTitle: HTMLElement;
   freeMaxEmailsPerMonth: HTMLElement;
   freeMaxEmailsPerDay: HTMLElement;
+  freeLink: HTMLAnchorElement;
   ppuTitle: HTMLElement;
   ppuPricePerEmailCents: HTMLElement;
   ppuMaxEmailsPerMonth: HTMLElement;
   ppuMaxEmailsPerDay: HTMLElement;
+  ppuLink: HTMLAnchorElement;
   sdeTitle: HTMLElement;
+  sdeLink: HTMLAnchorElement;
 }
 
 window && main();
