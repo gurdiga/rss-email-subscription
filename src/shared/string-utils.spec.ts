19581fbc62f005125bb24f6437a58d6494ea65be
Vlad GURDIGA <gurdiga@gmail.com>
2023-04-26 21:05:24 +0300

    string-utils: intro rawsi

--- src/shared/string-utils.spec.ts
+++ src/shared/string-utils.spec.ts
@@ -1,11 +1,25 @@
 import { expect } from 'chai';
-import { si } from './string-utils';
+import { rawsi, si } from './string-utils';
 
 describe(si.name, () => {
-  it('concatenates the given strings and requires interpolated expressions to be strings or numbers', () => {
+  it('typechecks to only allows number and string values to be interpolated', () => {
     const string = 'Yes!';
     const number = 42;
 
     expect(si`Does it work? ${string} ${number}`).to.equal('Does it work? Yes! 42');
+  });
+});
+
+describe(rawsi.name, () => {
+  it('has the name ending in "si" to pass lint-require-strict-interpolation', () => {
+    expect(rawsi.name.endsWith('si')).to.be.true;
+  });
+
+  it('screens backslashes in RegExp’s, exactly like String.raw', () => {
+    expect(new RegExp(rawsi`\+`)).to.deep.equal(new RegExp('\\+'));
+  });
+
+  it('typechecks to only allows number and string values to be interpolated', () => {
+    expect(rawsi`one: ${1}\ntwo: ${'doi'}`).to.equal('one: 1\\ntwo: doi');
   });
 });
--- src/shared/string-utils.ts
+++ src/shared/string-utils.ts
@@ -1,9 +1,25 @@
+type AllowedIterpolatedValueType = string | number;
+
 /**
  * Strict interpolation: the interpolated expressions are reqiured to be strings or numbers.
  *
  * Kudos to https://github.com/wongjiahau
  * https://github.com/microsoft/TypeScript/issues/30239#issuecomment-1132372523
  */
-export function si(strings: TemplateStringsArray, ...values: (string | number)[]): string {
-  return strings.reduce((result, string, index) => result + string + (values[index] ?? ''), '');
+export function si(strings: TemplateStringsArray, ...values: AllowedIterpolatedValueType[]): string {
+  return strings.reduce(interpolate(values), '');
+}
+
+/**
+ * Strict interpolation for raw strings: the interpolated expressions are reqiured to be strings or numbers.
+ *
+ * Kudos to Dr. Axel Rauschmayer
+ * https://exploringjs.com/es6/ch_template-literals.html#_example-stringraw
+ */
+export function rawsi(strings: TemplateStringsArray, ...values: AllowedIterpolatedValueType[]) {
+  return strings.raw.reduce(interpolate(values), '');
+}
+
+function interpolate(values: AllowedIterpolatedValueType[]) {
+  return (result: string, string: string, index: number) => result + string + (values[index] ?? '');
 }
