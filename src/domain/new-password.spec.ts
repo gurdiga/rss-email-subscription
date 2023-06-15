587c71161ef8c6131fc6853573d238f65e62d49b
Vlad GURDIGA <gurdiga@gmail.com>
2023-06-15 12:29:39 +0300

    domain/makeNewPassword: add field

--- src/domain/new-password.spec.ts
+++ src/domain/new-password.spec.ts
@@ -4,21 +4,22 @@ import { si } from '../shared/string-utils';
 import { makeNewPassword, maxPasswordLength, minPasswordLength } from './new-password';
 
 describe(makeNewPassword.name, () => {
+  const field = 'newPassword';
   const longEnoughPassword = '*'.repeat(minPasswordLength);
 
   it(si`rejects passwords shorter than ${minPasswordLength}`, () => {
-    expect(makeNewPassword('short password')).to.deep.equal(makeErr('Too short'));
+    expect(makeNewPassword('short password')).to.deep.equal(makeErr('Too short', field));
   });
 
   it(si`rejects passwords longer than ${maxPasswordLength}`, () => {
     const unacceptablyLongPassword = '*'.repeat(maxPasswordLength) + ' some more';
-    expect(makeNewPassword(unacceptablyLongPassword)).to.deep.equal(makeErr('Too long'));
+    expect(makeNewPassword(unacceptablyLongPassword)).to.deep.equal(makeErr('Too long', field));
   });
 
   it('rejects passwords with leading/trailing spaces', () => {
-    expect(makeNewPassword(si`  ${longEnoughPassword}`)).to.deep.equal(makeErr('Has leading spaces'));
-    expect(makeNewPassword(si`  ${longEnoughPassword}  `)).to.deep.equal(makeErr('Has leading spaces'));
-    expect(makeNewPassword('test  ')).to.deep.equal(makeErr('Has trailing spaces'));
+    expect(makeNewPassword(si`  ${longEnoughPassword}`)).to.deep.equal(makeErr('Has leading spaces', field));
+    expect(makeNewPassword(si`  ${longEnoughPassword}  `)).to.deep.equal(makeErr('Has leading spaces', field));
+    expect(makeNewPassword('test  ')).to.deep.equal(makeErr('Has trailing spaces', field));
   });
 
   it('accepts passwords containing spaces inside', () => {
--- src/domain/new-password.ts
+++ src/domain/new-password.ts
@@ -8,21 +8,21 @@ export interface NewPassword {
 export const minPasswordLength = 16;
 export const maxPasswordLength = 128;
 
-export function makeNewPassword(password: string): Result<NewPassword> {
+export function makeNewPassword(password: string, field = 'newPassword'): Result<NewPassword> {
   if (/^\s/.test(password)) {
-    return makeErr('Has leading spaces');
+    return makeErr('Has leading spaces', field);
   }
 
   if (/\s$/.test(password)) {
-    return makeErr('Has trailing spaces');
+    return makeErr('Has trailing spaces', field);
   }
 
   if (password.length < minPasswordLength) {
-    return makeErr('Too short');
+    return makeErr('Too short', field);
   }
 
   if (password.length > maxPasswordLength) {
-    return makeErr('Too long');
+    return makeErr('Too long', field);
   }
 
   return {
