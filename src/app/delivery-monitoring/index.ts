ce20eab96cadf06dbe7045b9b4df70a9a20fe45b
Vlad GURDIGA <gurdiga@gmail.com>
2023-06-27 11:04:33 +0300

    delmon: less frequent heartbeat

--- src/app/delivery-monitoring/index.ts
+++ src/app/delivery-monitoring/index.ts
@@ -30,8 +30,8 @@ async function main() {
 
   logInfo(si`Stared watching Postfix logs in ${process.env['NODE_ENV']!} environment`);
 
-  new CronJob('0 * * * *', () => {
-    logInfo('delmon hourly heartbeat', {
+  new CronJob('0 */3 * * *', () => {
+    logInfo('delmon heartbeat', {
       lineCount: status.lineCount,
       uptime: process.uptime(),
       memoryUsage: process.memoryUsage(),
