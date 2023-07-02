500cf266846073d0922d6155341ba6d6eabce0ae
Vlad GURDIGA <gurdiga@gmail.com>
2023-07-02 11:00:36 +0300

    delmon: mount /logs
    The problem I’m trying to solve with this change is delmon loosing the
    track of logs after the weekly rotation.
    
    It means giving delmon read access to more files than it needs, but
    the problem I’m trying to solve is more important than the risk.

--- src/app/delivery-monitoring/index.ts
+++ src/app/delivery-monitoring/index.ts
@@ -30,7 +30,8 @@ async function main() {
 
   logInfo(si`Stared watching Postfix logs in ${process.env['NODE_ENV']!} environment`);
 
-  new CronJob('0 */3 * * *', () => {
+  // new CronJob('0 */3 * * *', () => {
+  new CronJob('* * * * *', () => {
     logInfo('delmon heartbeat', {
       lineCount: status.lineCount,
       uptime: process.uptime(),
