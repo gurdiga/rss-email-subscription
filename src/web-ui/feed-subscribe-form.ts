eaafa1d5fa6904b018bc8f5567150e3f20352211
Vlad GURDIGA <gurdiga@gmail.com>
2023-07-18 13:31:04 +0300

    web-ui/feed-subscribe-form: add Link

--- src/web-ui/feed-subscribe-form.ts
+++ src/web-ui/feed-subscribe-form.ts
@@ -9,7 +9,8 @@ import {
   feedListBreadcrumbsLink,
   makeFeedManageBreadcrumbsLink,
 } from './breadcrumbs';
-import { displayInitError, requireQueryParams, requireUiElements } from './shared';
+import { createElement } from './dom-isolation';
+import { displayInitError, onClick, reportAppError, requireQueryParams, requireUiElements } from './shared';
 
 function main() {
   const queryStringParams = requireQueryParams<FeedSubscribeFormParams>({
@@ -32,6 +33,7 @@ function main() {
   const uiElements = requireUiElements<RequiredUiElements>({
     ...breadcrumbsUiElements,
     scriptContainer: '#script-container',
+    linkContainer: '#link-container',
   });
 
   if (isErr(uiElements)) {
@@ -39,12 +41,16 @@ function main() {
     return;
   }
 
-  const { scriptContainer } = uiElements;
+  const { scriptContainer, linkContainer } = uiElements;
 
   scriptContainer.value =
     si`<script res-subscription-form data-feed-id="${feedId.value}"` +
     si` src="${location.origin}/web-ui-scripts/web-ui/subscription-form.js">` +
     si`</script>`;
+
+  linkContainer.value = si`${location.origin}/to/${feedId.value}`;
+
+  addCopyButton(scriptContainer, linkContainer);
 
   displayBreadcrumbs(uiElements, [
     feedListBreadcrumbsLink,
@@ -55,8 +61,58 @@ function main() {
   ]);
 }
 
+function addCopyButton(...elements: HTMLTextAreaElement[]): void {
+  elements.forEach((el) => {
+    const parentStyle = getComputedStyle(el.parentElement!);
+    const parentIsPositioned = ['relative', 'absolute', 'fixed'].includes(parentStyle.position);
+
+    if (!parentIsPositioned) {
+      reportAppError(new Error(si`${addCopyButton.name} failed because of non-positioned element parent`));
+      return;
+    }
+
+    el.after(createCopyButton(el));
+  });
+}
+
+function createCopyButton(el: HTMLTextAreaElement): HTMLElement {
+  const buttonLabel = 'Copy';
+  const button = createElement('button', buttonLabel, {
+    class: 'btn btn-primary btn-sm opacity-50',
+  });
+
+  setStyle(button, {
+    position: 'absolute',
+    right: '.25rem',
+    transform: 'translateY(-110%)',
+    '--bs-btn-padding-y': '.25rem',
+    '--bs-btn-padding-x': '.5rem',
+    '--bs-btn-font-size': '.75rem',
+  });
+
+  onClick(button, () => {
+    el.select();
+    document.execCommand('copy');
+    button.textContent = 'Copied!';
+    el.selectionEnd = 0;
+
+    setTimeout(() => {
+      button.textContent = buttonLabel;
+    }, 1000);
+  });
+
+  return button;
+}
+
+function setStyle(el: HTMLElement, style: Record<string, string>): void {
+  for (const [prop, value] of Object.entries(style)) {
+    el.style.setProperty(prop, value);
+  }
+}
+
 interface RequiredUiElements extends BreadcrumbsUiElements {
   scriptContainer: HTMLTextAreaElement;
+  linkContainer: HTMLTextAreaElement;
 }
 
 main();
