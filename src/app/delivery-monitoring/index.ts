3ef9f90001ed06c3346523a82b28716884c378ec
Vlad GURDIGA <gurdiga@gmail.com>
2023-09-09 10:00:22 +0300

    delmon heartbeat: more concise

--- src/app/delivery-monitoring/index.ts
+++ src/app/delivery-monitoring/index.ts
@@ -1,4 +1,4 @@
-import { makeCustomLoggers } from '../../shared/logging';
+import { LoggerFunction, makeCustomLoggers } from '../../shared/logging';
 import { requireEnv } from '../../shared/env';
 import { AppEnv } from '../../api/init-app';
 import { makeStorage } from '../../domain/storage';
@@ -7,6 +7,7 @@ import { si } from '../../shared/string-utils';
 import { processData } from './line-processing';
 import { CronJob } from 'cron';
 import { DelmonStatus } from './delmon-status';
+import { humanSize } from '../../shared/number-utils';
 
 async function main() {
   const { logInfo, logWarning, logError } = makeCustomLoggers({ module: 'delivery-monitoring' });
@@ -28,15 +29,28 @@ async function main() {
   process.stdin.on('data', (data: Buffer) => processData(data, status, storage));
   process.stdin.on('end', () => logWarning('End of STDIN'));
 
-  logInfo(si`Started watching Postfix logs in ${process.env['NODE_ENV']!} environment`);
+  logInfo(si`Started watching Postfix logs in ${process.env['NODE_ENV'] || 'MISSING_NODE_ENV'} environment`);
 
   new CronJob('6 6 * * *', () => {
-    logInfo('delmon heartbeat', {
-      lineCount: status.lineCount,
-      uptimeDays: (process.uptime() / 3600 / 24).toFixed(),
-      memoryUsage: process.memoryUsage(),
-    });
+    logHeartbeat(logInfo, status);
   }).start();
 }
 
 main();
+
+function logHeartbeat(logInfo: LoggerFunction, status: DelmonStatus) {
+  logInfo('delmon heartbeat', {
+    lineCount: status.lineCount,
+    uptimeDays: (process.uptime() / 3600 / 24).toFixed(),
+    memoryUsage: memoryUsageStats(),
+  });
+}
+
+function memoryUsageStats() {
+  const usage = process.memoryUsage();
+
+  return {
+    rss: humanSize(usage.rss),
+    heap: humanSize(usage.heapUsed),
+  };
+}
